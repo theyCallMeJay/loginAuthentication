69bf949ee8888b33301a4765c52bd9fb912759e9
Jay <johnnywang7119@gmail.com>
2017-06-10 01:17:13 -0400

    first commit for login app

---
+++ app.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
+
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+//initialize app
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value){
+		var namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+
+		while(namespace.length){
+			formParam += '[' + namespace.shift() + ']';
+		}
+
+		return {
+			param: formParam,
+			msg: msg,
+			value, value
+		};
+
+	}
+}));
+
+app.use(flash());
+
+//global messages
+app.use(function(req, res, next){
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
+
+//define route files
+app.use('/', index);
+app.use('/users', users);
+
+app.set('port', (process.env.PORT || 3080));
+
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port: ' + app.get('port'));
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+module.exports = router;
